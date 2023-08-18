3d57d39ef919f6587798c24cae4f8bbfa48dc565
Barnabas Gonda <barnabasgonda@Barnabass-iMac.local>
2023-08-18 19:45:51 +0200

    good version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,10 @@ import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
 import {AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 // import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 // import { environment } from '../environments/environment';
@@ -51,8 +55,12 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    //AngularFireModule.initializeApp(environment.firebase),
+    //AngularFirestoreModule,
+    //provideFirebaseApp(() => initializeApp(environment.firebase)),
+    //provideAuth(() => getAuth()),
+    //provideDatabase(() => getDatabase()),
+    //provideFirestore(() => getFirestore())
     // provideFirebaseApp(() => initializeApp(environment.firebase)),
     // provideDatabase(() => getDatabase()),
     // provideFirestore(() => getFirestore())
